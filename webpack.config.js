44fe86f12ba6ad6301da9a873e5aaa6f37971bb7
N0FreeLunch <consilience90@gmail.com>
2023-08-30 00:43:20 +0900

    handlebar 예제 코드 추가

--- webpack.config.js
+++ webpack.config.js
@@ -68,6 +68,10 @@ const config = {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
                 type: 'asset',
             },
+            {
+                test: /\.hbs$/,
+                loader: "handlebars-loader"
+            }
 
             // Add your rules for custom modules here
             // Learn more about loaders from https://webpack.js.org/loaders/
